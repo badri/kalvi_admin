6760f54a38c2c00bffecd46c75ea308856b45450
lakshminp <badri.dilbert@gmail.com>
2014-02-12 23:08:07 +0530

    changed to bootstrap.

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 jQuery(document).ready(function(){
-    $("#tabs").tabs({
-            beforeLoad: function(event, ui) {
-                $('#preloader').show();
-            },
-            load: function(event, ui) {
-                $("#preloader").hide();
-            }
-    }).addClass( "ui-tabs-vertical ui-helper-clearfix" );
-$("#tabs li").removeClass("ui-corner-top").addClass("ui-corner-left");
+    jQuery(".tab").click(function(){
+	tabUrl = jQuery(this).attr("href");
+	jQuery(".tab").parent().removeClass("active");
+	jQuery(this).parent().addClass("active");
+	return false;
+    });
 });
